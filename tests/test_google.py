a9e604af3cd3aa64545e1d274a3efaacfaf046a4
Micheal Sachin <michealsachina@gmail.com>
2026-02-26 14:36:50 +0530

    test_google.py

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,9 +1,15 @@
 from playwright.sync_api import sync_playwright
 
-def test_google_title():
+def test_add_student():
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
-        page.goto("https://www.google.com")
-        assert "Google" in page.title()
+
+        page.goto("http://127.0.0.1:8000/students/add/")
+        page.fill("input[name='name']", "Rahul")
+        page.fill("input[name='age']", "20")
+        page.click("button[type='submit']")
+
+        assert "Rahul" in page.content()
+
         browser.close()
